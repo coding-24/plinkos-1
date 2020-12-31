f692946581bbcf0a51b91ed1567e138669f9cc4f
coding-24 <75677241+coding-24@users.noreply.github.com>
2020-12-31 18:51:48 +0530

    Add files via upload

--- Particle.js
+++ Particle.js
@@ -1,19 +1,30 @@
 class Particle {
-    constructor(x,y) {
-      var options = {
-          isStatic: true
+  constructor(x, y,r) {
+
+      var options ={
+          restitution:0.4
       }
-      this.body = Bodies.circle(x,y,10,options);
-      this.radius = 10;
-      this.color=color(random(0,255),random(0,255),random(0,255));
+      this.r=r;
+    
+      this.body = Bodies.circle(x, y, this.r,options);       
+      this.color=color(random(0, 255), random(0, 255), random(0, 255));
       World.add(world, this.body);
-    }
-    display(){
-      var pos =this.body.position;
-      var angle =this.body.angle;
+
+  }
+  display() {
+
+      var pos = this.body.position;
+      var angle = this.body.angle;
+
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      //imageMode(CENTER);
+      noStroke();
+      fill(this.color)
       ellipseMode(RADIUS);
-      fill(this.color);
-      //rect(pos.x, pos.y, this.width, this.height);
-      ellipse(0,0,this.radius,this.radius)
-    }
-  };
+      ellipse(0, 0, this.r,this.r);
+      pop();
+  }
+
+};
--- Plinko.js
+++ Plinko.js
@@ -1,17 +1,29 @@
 class Plinko {
-    constructor(x,y) {
+  constructor(x, y) {
       var options = {
-          isStatic: true
+          restitution: 1,
+          friction: 0,
+          isStatic:true
       }
-      this.body = Bodies.rectangle(x,y,10,options);
-      this.radius = 10;
+      this.r = 10;
+      this.body = Bodies.circle(x, y, this.r, options);
+      
       World.add(world, this.body);
-    }
-    display(){
-      var pos =this.body.position;
-      rectMode(CENTER);
-      fill("brown");
-      //rect(pos.x, pos.y, this.width, this.height);
-      ellipse(0,0,this.radius,this.radius)
-    }
-  };
+  }
+  display() {
+
+      var pos = this.body.position;
+      var angle = this.body.angle;
+
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      noStroke();
+      fill("white");
+      ellipseMode(RADIUS);
+      ellipse(0,0,this.r,this.r);
+      pop();
+  }
+
+};
